2744951ffc928a0e8f177d76ddce76e36e1dec3c
Abhijeet Dutta <93587547+abhijeetdutta3008@users.noreply.github.com>
2024-09-25 12:54:51 +0530

    Update App.jsx

--- adminPanel/src/App.jsx
+++ adminPanel/src/App.jsx
@@ -9,7 +9,7 @@ import ListAlbum from './pages/ListAlbum';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
-export const url = 'http://localhost:4000';
+export const url = 'https://audioavenue-backend.onrender.com';
 
 const App = () => {
   return (
